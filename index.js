8288af1252d3df29ffb0b36d1912b19eb115c8a1
darling_d <ddarling212@gmail.com>
2021-12-16 13:36:50 -0500

    Created function to display random greeting

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ btn.addEventListener("click", writeGreeting)
 const greetings = [
     "Santa Claus is coming to town!",
     "We wish you a Merry Christmas!",
-    "Happy holidays!",
+    "Happy Holidays!",
     "Ho, ho, ho! Merry Christmas!",
     "Jingle all the way!",
 ]
@@ -14,6 +14,11 @@ const greetings = [
 // Task:
 // Write a function to display a random greeting in the card.
 
+function writeGreeting() {
+        let randomGreeting = Math.floor(Math.random() * greetings.length);
+        greetingDisplay.innerText = greetings[randomGreeting];
+    }
+    
 // Stretch goals:
 // - Allow the user to input to and from names.
 // - Add an input for custom messages.
